b26bf25ba52f1b131650ccc8db42c8bf5fc4fb23
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2019-12-10 17:00:05 -0500

    Updates

---
+++ precache-manifest.4f96126c37ad2565fb64734ca536915f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6613aa4af5133b2e972f1945057c77",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "0a692e77e9a12c162578",
+    "url": "/pokemon-app/static/css/main.7d696823.chunk.css"
+  },
+  {
+    "revision": "bc7e11b98a795fceac92",
+    "url": "/pokemon-app/static/js/2.8c216a91.chunk.js"
+  },
+  {
+    "revision": "e57c559d1cf7fee52b5b",
+    "url": "/pokemon-app/static/js/3.c916e621.chunk.js"
+  },
+  {
+    "revision": "f207a03c5969a573d4a0",
+    "url": "/pokemon-app/static/js/4.ec4948f0.chunk.js"
+  },
+  {
+    "revision": "0a692e77e9a12c162578",
+    "url": "/pokemon-app/static/js/main.a6f8b808.chunk.js"
+  },
+  {
+    "revision": "66fbd90c75c898e9b7b3",
+    "url": "/pokemon-app/static/js/runtime-main.d6e271c5.js"
+  },
+  {
+    "revision": "5e448342de8263115aa757eaabef6ce3",
+    "url": "/pokemon-app/static/media/search.5e448342.svg"
+  }
+]);
