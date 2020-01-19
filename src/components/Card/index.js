829a9dbda54a22d0b59247a55986507faa3ad528
jose gonzalez <josegonzalez@MacBook-Pro-de-jose.local>
2020-01-18 23:27:48 -0500

    implement hooks in cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable no-shadow */
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
@@ -9,45 +9,43 @@ import {
   Card, Avatar, Description, Button,
 } from './styles';
 
-class PokeCard extends Component {
-  componentDidMount() {
-    const { fecthSpecies, name } = this.props;
-    fecthSpecies(name);
-  }
+const PokeCard = (props) => {
+  const element = useRef(null);
+  useEffect(() => {
+    // const { fecthSpecies, name } = props;
+    // fecthSpecies(name);
+  }, [element]);
 
-
-  render() {
-    const {
-      name, chain,
-    } = this.props;
-    if (!chain[name]) return null;
-    // const type = details[name].types
-    //   ? details[name].types.map((tp) => tp.type.name)
-    //   : ['default'];
-    const idChain = chain[name].evolution_chain.url.split('/')[
-      chain[name].evolution_chain.url.split('/').length - 2
-    ];
-    return (
-      <Card className="element" key={name}>
-        <Avatar src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt="" />
-        <Description>
-          <h3>{name}</h3>
-          <Button>
-            <NavLink
-              to={{ pathname: '/details/', state: { idChain, name } }}
-            >
+  const {
+    name,
+  } = props;
+  // if (!chain[name]) return null;
+  // const type = details[name].types
+  //   ? details[name].types.map((tp) => tp.type.name)
+  //   : ['default'];
+  // const idChain = chain[name].evolution_chain.url.split('/')[
+  //   chain[name].evolution_chain.url.split('/').length - 2
+  // ];
+  return (
+    <Card ref={element} className="element" key={name}>
+      <Avatar src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt="" />
+      <Description>
+        <h3>{name}</h3>
+        <Button>
+          <NavLink
+            to={{ pathname: '/details/', state: { name } }}
+          >
   Details
-            </NavLink>
-          </Button>
-        </Description>
-      </Card>
-    );
-  }
-}
+          </NavLink>
+        </Button>
+      </Description>
+    </Card>
+  );
+};
 PokeCard.propTypes = {
-  fecthSpecies: PropTypes.func.isRequired,
+  // fecthSpecies: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
-  chain: PropTypes.array.isRequired,
+  // chain: PropTypes.array.isRequired,
 };
 const mapStateToProps = (state) => ({
   loading: state.selectPokemonReducer.loading,
